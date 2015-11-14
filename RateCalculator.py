2287f8831cd466bdc7e11aee95b48f1edfbc212d
veloxid <bachmair@phys.ethz.ch>
2015-11-14 17:02:23 +0100

    finetuning and making the gui nicer

--- RateCalculator.py
+++ RateCalculator.py
@@ -10,6 +10,7 @@ from collections import OrderedDict
 
 ew = 10
 
+
 # ============================================
 # CLASS DEFINITION
 # ============================================
@@ -19,18 +20,19 @@ class Gui:
         self.root = Tk()
         self.entries = self.create_entries()
         self.labels = self.create_labels()
+        self.other_labels = self.creat_other_labels()
         self.names = self.create_names()
         self.var_results = self.create_var_results()
         self.results = self.create_results()
         self.close = Button(self.root, text='Close', command=self.root.destroy)
-        self.clear = Button(self.root, text='Clear', command=self.clear)
-        self.update = Button(self.root, text='Update', command=self.update_var_results)
+        self.clear_but = Button(self.root, text='Clear Rates', command=self.clear, width=8)
+        self.update = Button(self.root, text='Update', command=self.update_var_results, width=11)
         self.config_root()
         self.make_gui()
 
     def config_root(self):
         self.root.title('Flux Calculator')
-        self.root.geometry('+800-400') #530x250
+        self.root.geometry('+800-400')  # 530x250
 
     def clear(self):
         no = ['area1', 'area2']
@@ -38,22 +40,38 @@ class Gui:
             if key not in no:
                 box.delete(0, 'end')
 
-    def create_var_results(self):
+    @staticmethod
+    def create_var_results():
         dic = OrderedDict()
         dic['flux coinc'] = StringVar()
         dic['flux calc'] = StringVar()
         return dic
 
+    def creat_other_labels(self):
+        dic = OrderedDict()
+        dic['main'] = Label(self.root, text='Flux Calculator', font='font/Font 16 bold')
+        dic['result'] = Label(self.root, text='Result', font='font/Font 10 bold')
+        dic['rate'] = Label(self.root, text='Rates', font='font/Font 10 bold')
+        dic['area'] = Label(self.root, text='Unmasked Area', font='font/Font 10 bold')
+        return dic
+
     def update_var_results(self):
-        for1 = float(self.entries['for1'].get())
-        for2 = float(self.entries['for2'].get())
-        forc = float(self.entries['coinc'].get())
-        area1 = float(self.entries['area1'].get())
-        area2 = float(self.entries['area2'].get())
-        flux_coinc = 10. * forc / min(area1, area2)
-        flux_calc = 5. * (for1 / area1 + for2 / area2)
-        self.var_results['flux coinc'].set('{0:2.1f}'.format(flux_coinc))
-        self.var_results['flux calc'].set('{0:2.1f}'.format(flux_calc))
+        try:
+            for1 = float(self.entries['for1'].get())
+            for2 = float(self.entries['for2'].get())
+            forc = float(self.entries['coinc'].get())
+            area1 = float(self.entries['area1'].get())
+            area2 = float(self.entries['area2'].get())
+            flux_coinc = 10. * forc / min(area1, area2)
+            flux_calc = 5. * (for1 / area1 + for2 / area2)
+            self.var_results['flux coinc'].set('{0:2.1f}'.format(flux_coinc))
+            self.var_results['flux calc'].set('{0:2.1f}'.format(flux_calc))
+        except ValueError:
+            flux_coinc = 'ERROR'
+            flux_calc = 'ERROR'
+            self.var_results['flux coinc'].set(flux_coinc)
+            self.var_results['flux calc'].set(flux_calc)
+        # self.clear()
 
     def create_names(self):
         dic = OrderedDict()
@@ -78,27 +96,33 @@ class Gui:
 
     def create_labels(self):
         dic = OrderedDict()
-        dic['coinc'] = Label(self.root, text='FORC [Hz]')
-        dic['for1'] = Label(self.root, text='FOR 1 [Hz]')
-        dic['for2'] = Label(self.root, text='FOR 2 [Hz]')
-        dic['area1'] = Label(self.root, text='area 1 [mm^2]')
-        dic['area2'] = Label(self.root, text='area 2 [mm^2')
+        dic['coinc'] = Label(self.root, text='FORC [Hz]', anchor=E, width=11)
+        dic['for1'] = Label(self.root, text='FOR 1 [Hz]', anchor=E, width=11)
+        dic['for2'] = Label(self.root, text='FOR 2 [Hz]', anchor=E, width=11)
+        dic['area1'] = Label(self.root, text=u'area 1 [mm\u00B2]', anchor=E, width=11)
+        dic['area2'] = Label(self.root, text=u'area 2 [mm\u00B2]', anchor=E, width=11)
         return dic
 
     def make_gui(self):
-        i = 0
+        self.other_labels['main'].grid(columnspan=4, pady=10)
+        i = 2
+        self.other_labels['rate'].grid(row=1, columnspan=2, pady=8)
+        self.other_labels['result'].grid(row=1, column=3, pady=8)
         for label, entry in zip(self.labels.values(), self.entries.values()):
-            label.grid(row=i)
-            entry.grid(row=i, column=1)
+            k = i + 2 if i > 4 else i
+            label.grid(row=k)
+            entry.grid(row=k, column=1)
             i += 1
-        j = 0
-        for label, msg in zip(self.names.values(), self.results.values()):
-            label.grid(row=j, column=2, rowspan=2, pady=10, padx=12)
-            msg.grid(row=j, column=3, rowspan=2, pady=10, padx=12)
+        j = 2
+        for label, entry in zip(self.names.values(), self.results.values()):
+            label.grid(row=j, column=2, rowspan=2, padx=10)
+            entry.grid(row=j, column=3, rowspan=2, padx=10)
             j += 2
-        self.clear.grid(row=i + 1)
-        self.close.grid(row=i + 1, column=1)
-        self.update.grid(row=i + 1, column=2)
+        self.clear_but.grid(row=5, column=1, pady=10, sticky=N)
+        self.other_labels['area'].grid(row=6, columnspan=2, pady=8)
+        self.update.grid(row=6, column=3)
+        self.close.grid(row=i + 2, column=3, pady=8)
+
 
 if __name__ == '__main__':
     t = Gui()
